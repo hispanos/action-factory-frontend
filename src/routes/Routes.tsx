fa9cfaa2fb7d6a84b0e22c7dcf2a961d6b14b1f2
Mailer Martinez <mailermartinez@gmail.com>
2024-04-02 17:27:48 -0500

    add privates routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,22 +1,70 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import PrivatedRoutes from './PrivatedRoutes';
+import Suppliers from '../pages/Suppliers';
+import { createContext, useEffect, useState } from 'react';
+import { User } from '../interfaces/User';
+
+export type UserContextType = {
+  user: User | undefined;
+  setUser: React.Dispatch<React.SetStateAction<User | undefined>>;
+  isLogged: boolean;
+  setIsLogged: React.Dispatch<React.SetStateAction<boolean>>;
+  loadingLogin: boolean;
+};
+
+export const AppContext = createContext<UserContextType>({
+  setUser: () => {},
+  setIsLogged: () => {},
+  isLogged: false,
+  user: undefined,
+  loadingLogin: false,
+});
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-  ]);
+  {
+    path: '/',
+    element: <PrivatedRoutes />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/suppliers', element: <Suppliers /> },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 
 const Routes = () => {
-  return (
-    <RouterProvider router={router} />
-  )
-}
+  const [user, setUser] = useState<User>();
+  const [isLogged, setIsLogged] = useState(false);
+  const [loadingLogin, setLoadingLogin] = useState(true);
 
-export default Routes
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLogged(true);
+    }else {
+      setIsLogged(false);
+    }
+    setLoadingLogin(false);
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+        isLogged,
+        setIsLogged,
+        loadingLogin,
+      }}
+    >
+      <RouterProvider router={router} />
+    </AppContext.Provider>
+  );
+};
+
+export default Routes;
