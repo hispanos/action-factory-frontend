3e6e37fffc66bcf07d993ff05931d0b99c181c9c
Mailer Martinez <mailermartinez@gmail.com>
2024-04-02 13:57:52 -0500

    initial project

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+const Routes = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default Routes
