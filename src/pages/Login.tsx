887c0656f2b8eade90db564668f46af0cf17e0d4
eliasib <ecantor.2906@gmail.com>
2024-04-18 20:28:09 -0500

    login eliasib

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,11 @@
-import {
-  Alert,
-  Avatar,
-  Box,
-  Button,
-  Container,
-  CssBaseline,
-  ThemeProvider,
-  Typography,
-  createTheme,
-} from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import InputText from '../components/InputText';
 import { login } from '../services/authentication';
 import { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../routes/Routes';
 import { useNavigate } from 'react-router-dom';
-
-const defaultTheme = createTheme();
+import Loginn from './Loginn'; 
+ 
 
 type Inputs = {
   email: string;
@@ -43,76 +31,19 @@ const Login = () => {
       setUser(response.user);
       setIsLogged(true);
       navigate('/');
-    }else {
+    } else {
       setErrorLogin(true);
     }
   };
 
   useEffect(() => {
-    if(isLogged) {
-        navigate('/')
+    if (isLogged) {
+      navigate('/')
     }
   }, [isLogged])
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h3">
-            Action Factory
-          </Typography>
-          <Typography component="h1" variant="h5">
-            Iniciar Sesión
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit(onSubmit)}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <InputText
-              type="text"
-              name="email"
-              control={control}
-              label="Correo electrónico"
-              required
-              errors={errors}
-            />
-            <InputText
-              type="password"
-              name="password"
-              control={control}
-              label="Contraseña"
-              required
-              errors={errors}
-            />
-            {
-              errorLogin &&
-              <Alert severity="error">Usuario o contraseña incorrectos.</Alert>
-            }
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Ingresar
-            </Button>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+    <Loginn /> 
   );
 };
 
