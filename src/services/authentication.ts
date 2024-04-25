f9dca530ea5e6d3e6b6457fed448dec9fce4cb4b
Mailer Martinez <mailermartinez@gmail.com>
2024-04-25 18:04:19 -0500

    conecction with backend

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { LoginResponse } from './types/login';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_LOGIN_URL;
 
 export const login = async (email: string, password: string) => {
   try {
