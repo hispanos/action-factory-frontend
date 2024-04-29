8ad06128caeecec2b6dfa7101e04d8d76039ec8c
Mailer Martinez <mailermartinez@gmail.com>
2024-04-28 20:34:31 -0500

    add sorting table

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -6,6 +6,7 @@ import {
   TableHead,
   TablePagination,
   TableRow,
+  TableSortLabel,
 } from '@mui/material';
 import { Columns, RowType, Rows } from '../interfaces/Table';
 import { useState } from 'react';
@@ -19,6 +20,48 @@ const TableComponent = <T extends RowType>({
 }) => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+  const [orderBy, setOrderBy] = useState<string | number>('');
+
+  const handleSortRequest = (property: keyof RowType) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function getComparator<Key extends keyof RowType>(
+    order: 'asc' | 'desc',
+    orderBy: Key
+  ): (
+    a: { [key in Key]: number | string },
+    b: { [key in Key]: number | string }
+  ) => number {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+
   return (
     <TableContainer>
       <Table size="small" stickyHeader>
@@ -27,16 +70,36 @@ const TableComponent = <T extends RowType>({
             {columns.map((column) => (
               <TableCell
                 key={column.id}
-                align={column.align}
-                style={{ minWidth: column.minWidth }}
+                sortDirection={orderBy === column.id ? order : false}
               >
-                {column.label}
+                <TableSortLabel
+                  active={orderBy === column.id}
+                  direction={orderBy === column.id ? order : 'asc'}
+                  onClick={() => handleSortRequest(column.id)}
+                >
+                  {column.label}
+                </TableSortLabel>
               </TableCell>
             ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows
+          {stableSort(rows, getComparator(order, orderBy))
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((row, index) => (
+              // Renderiza tus filas aquí
+              <TableRow key={index}>
+                {columns.map((column, index2) => {
+                  if (column.format) {
+                    return (
+                      <TableCell key={index2}>{column.format(row)}</TableCell>
+                    );
+                  }
+                  return <TableCell key={index2}>{row[column.name]}</TableCell>;
+                })}
+              </TableRow>
+            ))}
+          {/* {rows
             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
             .map((row, index) => (
               <TableRow key={index}>
@@ -49,7 +112,7 @@ const TableComponent = <T extends RowType>({
                   return <TableCell key={index2}>{row[column.name]}</TableCell>;
                 })}
               </TableRow>
-            ))}
+            ))} */}
         </TableBody>
       </Table>
       <TablePagination
